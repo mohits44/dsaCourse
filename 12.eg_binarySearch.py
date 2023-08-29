6f249035416b8db5efc8a31a1c363a9e8b0348dc
mohits44 <101943239+mohits44@users.noreply.github.com>
2023-08-29 19:57:11 +0530

    Update 12.eg_binarySearch.py

--- 12.eg_binarySearch.py
+++ 12.eg_binarySearch.py
@@ -2,7 +2,7 @@ def bs():
   n=5
   s=0
   e=n-1
-  mid=s+e//2
+  mid=s+e//2     #later replace with s+(e-s//2)
   a=[2,4,6,8,10]
   key=2
   while s<=e:
@@ -13,7 +13,7 @@ def bs():
         s=mid
       else:
         e=mid
-    mid=(s+e)//2
+    mid=(s+e)//2  #later replace with s+(e-s//2)
   return -1
 print(bs())
      
