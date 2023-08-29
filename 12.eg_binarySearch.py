a0510b66eddc3d1f42a394e1701b58bf24a57f6a
mohits44 <101943239+mohits44@users.noreply.github.com>
2023-08-29 19:34:13 +0530

    Create 12.eg_binarySearch.py

---
+++ 12.eg_binarySearch.py
@@ -0,0 +1,20 @@
+def bs():
+  n=5
+  s=0
+  e=n-1
+  mid=s+e//2
+  a=[2,4,6,8,10]
+  key=2
+  while s<=e:
+    if key==a[mid]:
+      return mid
+    else:
+      if key>a[mid]: #right dabba
+        s=mid
+      else:
+        e=mid
+    mid=(s+e)//2
+  return -1
+print(bs())
+     
+      
