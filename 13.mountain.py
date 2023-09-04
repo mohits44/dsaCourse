90094c1811746421d7d04cd40302f2905e67c247
mohits44 <101943239+mohits44@users.noreply.github.com>
2023-09-04 18:58:28 +0530

    Update 13.mountain.py

--- 13.mountain.py
+++ 13.mountain.py
@@ -1,19 +1,16 @@
-# Online Python-3 Compiler (Interpreter)
-def bs():
-  n=5
-  s=0
-  e=n-1
-  m=s+e//2     #later replace with s+(e-s//2)
-  a=[2,8,6,4,2]
-  
-  while s<=e:
-    if a[m]>a[m-1] and a[m]>a[m+1]:
-      return m
-    elif a[m-1]<a[m] and a[m]<a[m+1]:
-        s=m+1
-    elif a[m-1]>a[m] and a[m]>a[m+1]:
-        e=m
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+      s=0
+      e=len(arr)-1
+      m=(s+e)//2
+
+      while s<=e:
+        if arr[m]>arr[m-1] and arr[m]>arr[m+1]:
+          return m
+        elif arr[m-1]<arr[m] :
+          s=m+1
+        elif  arr[m]>arr[m+1]:
+          e=m
     
-    m=(s+e)//2  #later replace with s+(e-s//2)
-  return -1
-print(bs())
+        m=(s+e)//2  #later replace with s+(e-s//2)
+      return -1
