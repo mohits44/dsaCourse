8897e98e4b7940e744b6d1ca962dfc1b2332a52b
mohits44 <101943239+mohits44@users.noreply.github.com>
2023-09-11 10:49:47 +0530

    Update 12.eg_binarySearch.py

--- 12.eg_binarySearch.py
+++ 12.eg_binarySearch.py
@@ -1,20 +1,21 @@
-def bs():
-  n=5
-  s=0
-  e=n-1
-  mid=s+e//2     #later replace with s+(e-s//2)
-  a=[2,4,6,8,10]
-  key=2
-  while s<=e:
-    if key==a[mid]:
-      return mid
-    else:
-      if key>a[mid]: #right dabba
-        s=mid
-      else:
-        e=mid
-    mid=(s+e)//2  #later replace with s+(e-s//2)
-  return -1
-print(bs())
-     
-      
+def bs(arr, key):
+    n=len(arr)
+    low=0
+    high=n-1
+    
+ 
+    while low<=high:
+        mid=(low+high)//2     #later replace with low+(high-low//2)
+        if key==arr[mid]:
+            return mid
+        else:
+            if key>arr[mid]: #right dabba
+                low=mid+1
+            else:
+                high=mid-1
+   
+    return -1
+  
+a=[2,4,6,8,10]
+key=10
+print(bs(a,key))
